c7642a216bb816af2b5942aa1f73e4b2be2db371
Joao Bernardo Luis <joao.ber.luis@gmail.com>
2021-03-04 17:10:56 +0000

    chore: Added Compatibility for ActionBar.
    Integrated with Jet Pack Navigation.

--- app/src/main/java/com/beatstars/testingjetpacknavigation/MainActivity.kt
+++ app/src/main/java/com/beatstars/testingjetpacknavigation/MainActivity.kt
@@ -3,48 +3,44 @@ package com.beatstars.testingjetpacknavigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
+        setSupportActionBar(main_toolbar)
         val navHostFragment =
                 supportFragmentManager.findFragmentById(R.id.main_nav_fragment) as NavHostFragment
         val navController: NavController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         // Setup Bottom Navigation
-//        bottom_navigation.setupWithNavController(navController)
-
-        // Setup Top Navigation
-//        val appBarConfiguration = AppBarConfiguration(
-//                topLevelDestinationIds = setOf (
-//                        R.id.fragmentHome,
-//                        R.id.fragmentNotifications,
-//                        R.id.fragmentMenu
-//                )
-//        )
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        bottomNavigationView.apply {
+        bottom_navigation.apply {
             setupWithNavController(navController)
-//            // Note: This adds navigation like youtube and instagram.
-//            setOnNavigationItemSelectedListener {
-//                item -> onNavDestinationSelected(item, navController)
-//            }
         }
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Setup Action Bar Navigation
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf (
+                R.id.fragmentHome,
+                R.id.fragmentNotifications,
+                R.id.fragmentMenu
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     /**
      * Note:
      * We have overridden the onSupportNavigateUp() method, this will delegate the system up button call with the NavController.
      */
-//    override fun onSupportNavigateUp() = findNavController(R.id.main_nav_fragment).navigateUp()
+    override fun onSupportNavigateUp() = findNavController(R.id.main_nav_fragment).navigateUp()
 }
