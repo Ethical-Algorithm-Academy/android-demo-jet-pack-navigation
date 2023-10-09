5127bdbcdbdd5854a47acc9b92c72492b3e71301
Joao Luis <bernard.luis@necam.com>
2023-10-09 22:20:49 +0100

    refactor: Home Fragment changed Naming and notes to ID Nav;

--- app/src/main/java/eu/jobernas/jetpacknavigation/ui/id/IdNavigationFragment.kt
+++ app/src/main/java/eu/jobernas/jetpacknavigation/ui/id/IdNavigationFragment.kt
@@ -1,4 +1,4 @@
-package eu.jobernas.jetpacknavigation.ui.home
+package eu.jobernas.jetpacknavigation.ui.id
 
 import android.graphics.Color
 import android.os.Bundle
@@ -8,32 +8,32 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import eu.jobernas.jetpacknavigation.R
-import eu.jobernas.jetpacknavigation.databinding.FragmentHomeBinding
+import eu.jobernas.jetpacknavigation.databinding.FragmentIdBinding
 import eu.jobernas.jetpacknavigation.helpers.NavigationConfig
 import eu.jobernas.jetpacknavigation.models.Moto
 import eu.jobernas.jetpacknavigation.ui.details.MotoDetailsFragmentArgs
 
-class HomeFragment : Fragment(), View.OnClickListener {
+class IdNavigationFragment : Fragment(), View.OnClickListener {
 
     companion object {
-        fun newInstance() = HomeFragment()
+        fun newInstance() = IdNavigationFragment()
     }
 
-    private var binding: FragmentHomeBinding? = null
+    private var binding: FragmentIdBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentIdBinding.inflate(inflater, container, false)
         binding?.apply {
-            homeActionButton.setOnClickListener(this@HomeFragment)
+            idActionButton.setOnClickListener(this@IdNavigationFragment)
         }
         return binding?.root
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.home_action_button -> {
+            R.id.id_action_button -> {
                 // Calling Fragment By ID with Args Auto Generated
                 val selectedMoto = Moto("32", "DT", Color.WHITE, "Yamaha", false)
                 val bundle = MotoDetailsFragmentArgs(selectedMoto).toBundle()
